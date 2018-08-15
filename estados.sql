d86a6b4b53f1180ddf3c0e552d77e8127cd8446c
daniel <daniel@desconn.com.br>
2018-08-15 11:33:23 -0300

    commit inicial

---
+++ estados.sql
@@ -0,0 +1,37 @@
+CREATE TABLE estados(
+  id   INT NOT NULL,
+  nome  VARCHAR(50) NOT NULL,
+  sigla    CHAR(2) NOT NULL,
+  regiao  INT NOT NULL,
+  PRIMARY KEY(id)
+);
+
+INSERT INTO estados (id, nome, sigla, regiao)
+VALUES
+  (12, "Acre", "AC", 1),
+  (27, "Alagoas", "AL", 2),
+  (16, "Amapá", "AP", 1),
+  (13, "Amazonas", "AM", 1),
+  (29, "Bahia", "BA", 2),
+  (23, "Ceará", "CE", 2),
+  (53, "Distrito Federal", "DF", 5),
+  (32, "Espírito Santo", "ES", 3),
+  (52, "Goiás", "GO", 5),
+  (21, "Maranhão", "MA", 2),
+  (51, "Mato Grosso", "MT", 5),
+  (50, "Mato Grosso do Sul", "MS", 5),
+  (31, "Minas Gerais", "MG", 3),
+  (15, "Pará", "PA", 1),
+  (25, "Paraíba", "PB", 2),
+  (41, "Paraná", "PR", 4),
+  (26, "Pernambuco", "PE", 2),
+  (22, "Piauí", "PI", 2),
+  (33, "Rio de Janeiro", "RJ", 3),
+  (24, "Rio Grande do Norte", "RN", 2),
+  (43, "Rio Grande do Sul", "RS", 4),
+  (11, "Rondônia", "RO", 1),
+  (14, "Roraima", "RR", 1),
+  (42, "Santa Catarina", "SC", 4),
+  (35, "São Paulo", "SP", 3),
+  (28, "Sergipe", "SE", 2),
+  (17, "Tocantins", "TO", 1);
